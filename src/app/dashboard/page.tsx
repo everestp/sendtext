36c99a39f64926a92b0a9d25cc1a1c1693ecd02f
Everest Paudel <paudeleverest09@gmail.com>
2025-07-28 12:32:34 +0545

    chat-nextjs

---
+++ src/app/dashboard/page.tsx
@@ -0,0 +1,15 @@
+import { FC } from "react";
+
+interface pageProps {
+    
+}
+ 
+const page: FC<pageProps> = () => {
+    return ( 
+        <div>
+            Hello world
+        </div>
+     );
+}
+ 
+export default page;
